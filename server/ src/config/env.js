bde90d73638ade20a3b0a03697068e9da544de7b
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-08 07:35:50 +0100

    Create          env.js

---
+++ server/ src/config/env.js
@@ -0,0 +1,4 @@
+# MindsEye Server Environment Configuration
+
+PORT=4000
+MONGODB_URI=mongodb+srv://peacethabibinflow_db_user:hAQ8nhCFCXD3yJd6@cluster0.xavgxkm.mongodb.net/mindseye?retryWrites=true&w=majority&appName=Cluster0
